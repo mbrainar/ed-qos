356cd5aaf2ba65d55430fe4216d2037357a5d632
Matthew Brainard <mbrainar@cisco.com>
2016-08-19 11:04:40 -0400

    Added function to get application id

--- apic.py
+++ apic.py
@@ -19,16 +19,32 @@ import os
 
 
 def get_ticket():
-	apic = os.environ.get('APIC_SERVER')
-	username = os.environ.get('APIC_USERNAME')
-	password = os.environ.get('APIC_PASSWORD')
+    apic = os.environ.get('APIC_SERVER')
+    username = os.environ.get('APIC_USERNAME')
+    password = os.environ.get('APIC_PASSWORD')
 
-	reqUrl = "https://{0}/api/v1/ticket".format(apic)
-	payload = {'username': username, 'password': password}
+    reqUrl = "https://{0}/api/v1/ticket".format(apic)
+    payload = {'username': username, 'password': password}
 
-	r = requests.post(reqUrl, json=payload)
+    r = requests.post(reqUrl, json=payload)
 
-	if (r.status_code == 200):
-		return r.json()[u'response'][u'serviceTicket']
-	else:
-		r.raise_for_status()
+    if (r.status_code == 200):
+        return r.json()[u'response'][u'serviceTicket']
+    else:
+        r.raise_for_status()
+
+
+def get_app_id(service_ticket, app_name):
+    apic = os.environ.get('APIC_SERVER')
+
+    reqUrl = "https://{0}/api/v1/application?name={1}".format(apic, app_name)
+    header = {"X-Auth-Token": service_ticket}
+
+    r = requests.get(reqUrl, headers=header)
+
+    if (r.status_code == 200):
+        return r.json()['response'][0]['id']
+    else:
+        r.raise_for_status()
+
+#print get_app_id(get_ticket(), "facebook")
