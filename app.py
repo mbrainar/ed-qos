34ef4338b55f3545a9a518c5ffb18b432fb97db9
Steve Luzynski <steve@luzynski.net>
2016-08-29 11:39:46 -0500

    first hack at a sensible UI

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from flask import render_template
 import apic
 import weather
 
 app = Flask(__name__)
+event_status = False
 
 
 @app.route('/')
 def hello_world():
-    return "This is the event driven Qos Application. Please see http://github.com/imapex/ed-qos for more information"
+    # call the apic.py module to get some details on what is currently in play
 
+    return render_template('index.html')
 
 @app.route('/apic-test/')
 def check_ticket():
@@ -16,11 +19,13 @@ def check_ticket():
     answer_string = "ticket:" + ticket + " appId: " + apic.get_appid(ticket)
     return answer_string
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
 
 @app.route('/status/')
 def get_status():
-    # ticket = apic.get_ticket()
-    if (1):
+    if (event_status == False):
         answer_string = "All is clear!"
     else:
         answer_string = "Currently in emergency state"
@@ -44,6 +49,12 @@ def event_off():
     service_ticket = apic.get_ticket()
     return apic.put_policy_update(service_ticket,apic.update_app_state(event_status,apic.get_policy(service_ticket,policy_scope),apic.get_app_id(service_ticket,app_name),app_name),policy_scope)
 
+@app.route('/event/toggle/')
+def event_toggle():
+    if (event_status == False):
+        return redirect(url_for('/event/on/'))
+    else:
+        return redirect(url_for('/event/off/'))
 
 @app.route('/weather/')
 def check_weather():
