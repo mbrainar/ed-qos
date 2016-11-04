aa7ff3da7550be8a5d3a21289eae1ec7d626d482
Matthew Brainard <mbrainar@cisco.com>
2016-11-04 12:19:48 -0400

    added documentation. fixed ssl verification issues. updated functions to work with changes in APIC 1.3

--- apic.py
+++ apic.py
@@ -16,16 +16,18 @@ import json
 
 # Define global variables
 apic = os.environ.get("APIC_SERVER")
+username = os.environ.get('APIC_USERNAME')
+password = os.environ.get('APIC_PASSWORD')
 
 # Get the service ticket to be used in API calls
-def get_ticket():
-    username = os.environ.get('APIC_USERNAME')
-    password = os.environ.get('APIC_PASSWORD')
+def get_ticket(username, password):
+    # username = os.environ.get('APIC_USERNAME')
+    # password = os.environ.get('APIC_PASSWORD')
 
     reqUrl = "https://{0}/api/v1/ticket".format(apic)
     payload = {'username': username, 'password': password}
 
-    r = requests.post(reqUrl, json=payload)
+    r = requests.post(reqUrl, json=payload, verify=False)
 
     if (r.status_code == 200):
         return r.json()[u'response'][u'serviceTicket']
@@ -38,7 +40,7 @@ def get_app_id(service_ticket, app_name):
     reqUrl = "https://{0}/api/v1/application?name={1}".format(apic, app_name)
     header = {"X-Auth-Token": service_ticket}
 
-    r = requests.get(reqUrl, headers=header)
+    r = requests.get(reqUrl, headers=header, verify=False)
 
     if (r.status_code == 200):
         return r.json()['response'][0]['id']
@@ -51,7 +53,7 @@ def get_policy(service_ticket, policy_scope):
     reqUrl = "https://{0}/api/v1/policy?policyScope={1}".format(apic, policy_scope)
     header = {"X-Auth-Token": service_ticket}
 
-    r = requests.get(reqUrl, headers=header)
+    r = requests.get(reqUrl, headers=header, verify=False)
 
     if r.status_code == 200:
         return r.json()
@@ -90,12 +92,12 @@ def update_app_state(service_ticket, event_status, policy, app_list):
                     # If we are looking at the business-irrelevant (or default) policy, remove the specified application
                     else:
                         # Loop through each of the applications in the business-irrelevant policy
-                        for a in range(len(policy['response'][i]['resource']['applications'])):
+                        for app in policy['response'][i]['resource']['applications']:
                             # If app matches, delete from business-irrelevant, else continue looping applications
-                            try:
-                                policy['response'][i]['resource']['applications'].remove({'id': app_id, 'appName': app_name})
+                            if app["appName"] == app_name:
+                                policy['response'][i]['resource']['applications'].remove(app)
                                 print "Removed {0} from business-irrelevant".format(app_name)
-                            except:
+                            else:
                                 continue
             else:
                 continue
@@ -119,13 +121,15 @@ def update_app_state(service_ticket, event_status, policy, app_list):
                     # If we are looking at the business-relevant (or default) policy, remove the specified application
                     else:
                         # Loop through each of the applications in the business-relevant (and default)
-                        for a in range(len(policy['response'][i]['resource']['applications'])):
+                        for app in policy['response'][i]['resource']['applications']:
                             #if app matches, delete from business-relevant, else continue looping applications
-                            try:
-                                policy['response'][i]['resource']['applications'].remove({'id': app_id, 'appName': app_name})
+                            if app["appName"] == app_name:
+                                policy['response'][i]['resource']['applications'].remove(app)
                                 print "Removed {0} from business-relevant".format(app_name)
-                            except:
+                            else:
+                                print "not removed"
                                 continue
+
             else:
                 continue
         return policy
@@ -136,7 +140,7 @@ def put_policy_update(service_ticket, policy, policy_scope):
         reqUrl = "https://{0}/api/v1/policy".format(apic)
         header = {"X-Auth-Token": service_ticket, "Content-type":"application/json"}
 
-        r = requests.put(reqUrl, headers=header, json=policy['response'])
+        r = requests.put(reqUrl, headers=header, json=policy['response'], verify=False)
 
         if r.status_code == 202:
             task_id = r.json()['response']['taskId']
@@ -154,7 +158,7 @@ def get_task(service_ticket, task_id):
     reqUrl = "https://{0}/api/v1/task/{1}".format(apic, task_id)
     header = {"X-Auth-Token": service_ticket, "Content-type": "application/json"}
 
-    r = requests.get(reqUrl, headers=header)
+    r = requests.get(reqUrl, headers=header, verify=False)
 
     if r.status_code == 200:
         return r.json()
@@ -167,7 +171,7 @@ def get_policy_scope(service_ticket):
     reqUrl = "https://{0}/api/v1/policy/tag".format(apic)
     header = {"X-Auth-Token": service_ticket}
 
-    r = requests.get(reqUrl, headers=header)
+    r = requests.get(reqUrl, headers=header, verify=False)
 
     if r.status_code == 200:
         list = []
@@ -184,7 +188,7 @@ def get_applications(service_ticket, policy_scope):
     reqUrl = "https://{0}/api/v1/policy?policyScope={1}".format(apic, policy_scope)
     header = {"X-Auth-Token": service_ticket}
 
-    r = requests.get(reqUrl, headers=header)
+    r = requests.get(reqUrl, headers=header, verify=False)
 
     if r.status_code == 200:
         list = []
@@ -202,9 +206,13 @@ def get_applications(service_ticket, policy_scope):
 
 '''
 #Code test block
+apic="10.10.10.111"
+username="admin"
+password="1vtG@lw@y"
+
 policy_scope = "ed-qos"
-app_list = ["facebook","netflix","facetime"]
-service_ticket = get_ticket()
+app_list = ["facebook"]
+service_ticket = get_ticket(username,password)
 
 #print get_app_state(get_policy(get_ticket(),policy_scope),get_app_id(get_ticket(),app_name),app_name)
 
@@ -217,5 +225,5 @@ new.write(json.dumps(update_app_state(service_ticket,True,get_policy(service_tic
 old.close()
 new.close()
 
-print put_policy_update(service_ticket,update_app_state(service_ticket,False,get_policy(service_ticket,policy_scope),app_list),policy_scope)
+#print put_policy_update(service_ticket,update_app_state(service_ticket,False,get_policy(service_ticket,policy_scope),app_list),policy_scope)
 '''
